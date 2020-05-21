46b843a91d79fdbda16a51618f2fa03a3f3eedf8
Jinit Jain <jinitmjain15698@gmail.com>
2020-05-21 23:06:01 +0530

    plot_map.py updated

--- correlation-modules/plot_map.py
+++ correlation-modules/plot_map.py
@@ -1,9 +1,9 @@
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 def plot_map (xPoints):
-    x = xPoints
-    x.sort()
-    plt.hist(x, bins=20)
-    plt.gca().set(title='Correlation Distribution', ylabel='Frequency')
-    plt.xticks(rotation=90, ha='right')
+    sns.set(color_codes=True)
+    sns.distplot(xPoints, bins=20)
     plt.show()
+
+
